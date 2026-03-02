66520dad05ee629733107bad4ac7d08e91161aab
trannkiennquangg-netizen <trannkiennquangg@gmail.com>
2026-03-02 20:58:36 +0700

    up

--- assets/script/footer.js
+++
@@ -1,5 +0,0 @@
-fetch('./assets/footer.html')
-.then(response => response.text())
-.then(data => {
-    document.getElementById('footer1').innerHTML = data;
-});
--- assets/script/header.js
+++
@@ -1,5 +0,0 @@
-fetch('./assets/header.html')
-.then(response => response.text())
-.then(data => {
-    document.getElementById('header1').innerHTML = data;
-});
